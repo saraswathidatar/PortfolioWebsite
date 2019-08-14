64c0c006a4ba5d420d4fb5b561bb5a8585762c1e
sara <saraswathidatar@gmail.com>
2019-08-14 13:34:38 -0700

    edited about me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -9,7 +9,7 @@ class About extends Component {
            class="center"
            />
 
-           <p class="aboutme-paragraph">My name is Saraswathi Datar and I am currently a senior at Portland State University
+           <p class="aboutme-paragraph">My name is Saraswathi Datar and I am currently a Student at Portland State University
            pursuing a Masters in Computer Science. Languages known to me are C,C#,Java,Rust,HTML/CSS. I am interested in security. I have completed computer security course which dealt with smashing the stack,code injection exploits,defeating simple password storage schemes. Further improving my security skills with web security that had us perform SQL injections,code injection,command injection,XML and XSS injections. Moving in that direction I learnt Rust programming language which is memory safe and has trustworthy concurrency. I also have an experience in web development and Android software development.
 
            </p>
